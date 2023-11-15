c2ffd663f186b359554d16469d66e22b0cffbf32
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-11-14 21:06:32 -0600

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   //@ts-nocheck
 	import { onMount } from 'svelte';
+  import { Card } from 'flowbite-svelte';
 
   // export let movieData;
   export let data;
@@ -12,11 +13,20 @@
   });
 </script>
 
-<h1>Upcoming Movies</h1>
 
 
-<div>
-  {#each data.data as movie}
-    <div>Title: {movie.title} - {movie.streaming_date}</div>
-  {/each}
+
+<div class="bg-[url($lib/images/theater.jpg)] min-h-screen bg-cover overflow-hidden bg-no-repeat">
+  <h1 class="font-serif text-gray-300 font-bold mx-auto place-items-center text-3xl">Upcoming Movies</h1>
+  <div class="dark">
+    {#each data.data as movie}
+      <div class="py-1 px-1">
+        <Card href="/cards" class="">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.title}</h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{movie.streaming_date}</p>
+        </Card>
+      </div>
+    {/each}
+  </div>
+
 </div>
