1a7b87afb94a70368eac1d34d394c471db6f5eac
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-11-16 21:48:28 -0600

    links to imdb now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,14 +20,17 @@
   <h1 class="font-serif text-gray-200 font-bold flex flex-row justify-center py-5 text-3xl">Upcoming Movies</h1>
   <div class="dark flex flex-wrap justify-evenly">
     {#each data.data as movie}
-      <div class="py-1 px-1 flex justify-center">
-        <Card img={movie.postersrc} href="/" horizontal reverse={hCard} class="mb-4">
-          <div class="min-w-[350px] max-w-[350px]">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.title}</h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{movie.streaming_date}</p>
-          </div>
-        </Card>
-      </div>
+        <div class="py-1 px-1 flex justify-center">
+          <Card img={movie.postersrc} href={`https://www.imdb.com/find/?q=${encodeURIComponent(movie.title)}&ref_=nv_sr_sm`} horizontal reverse={hCard} class="mb-4">
+            <div class="min-w-[350px] max-w-[350px]">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.title}</h5>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{movie.streaming_date}</p>
+            </div>
+          </Card>
+        </div>
     {/each}
   </div>
 </div>
+
+
+<!-- https://www.imdb.com/find/?q={}&ref_=nv_sr_sm -->
