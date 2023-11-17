bc536f37ca1876b8b005dd846bd74a09bf57b26f
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-11-16 21:16:30 -0600

    posters added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,20 +21,13 @@
   <div class="dark">
     {#each data.data as movie}
       <div class="py-1 px-1 flex justify-center">
-        <Card href="/cards" class="max-w-[225px]">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.title}</h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{movie.streaming_date}</p>
+        <Card img={movie.postersrc} href="/" horizontal reverse={hCard} class="mb-4">
+          <div class="max-w-[300px]">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.title}</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">{movie.streaming_date}</p>
+          </div>
         </Card>
       </div>
     {/each}
   </div>
-</div>
-
-
-<div>
-  <Card img="/images/image-1.webp" href="/" horizontal reverse={hCard} class="mb-4">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-  </Card>
-  <Toggle bind:checked={hCard} class="italic dark:text-gray-500">Reverse</Toggle>
 </div>
