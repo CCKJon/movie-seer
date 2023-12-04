d105151011eaf26d8d25f97250bbeec1d8f9f3e5
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-12-03 21:58:45 -0600

    updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,29 @@
 </script>
 
 <div class="bg-[url($lib/images/theater.jpg)] min-h-screen bg-cover overflow-hidden bg-no-repeat">
-  <div class="flex flex-row justify-evenly items-center py-10 ">
-	<a href="/" class="font-serif text-gray-200 flex flex-row justify-center text-5xl hover:underline {$page.url.pathname==='/' ? "underline":""}">Home</a>
-    <a href="/streaming" class="font-serif text-gray-200 flex flex-row justify-center text-5xl hover:underline {$page.url.pathname==='/streaming' ? "underline":""}">Upcoming Streamed Movies</a>
-    <a href="/theater" class="font-serif text-gray-200 flex flex-row justify-center text-5xl hover:underline {$page.url.pathname==='/theater' ? "underline":""}">Upcoming Movies in Theaters</a>
-  </div>
+	<div class="flex flex-row justify-evenly items-center py-10">
+		<a
+			href="/"
+			class="font-serif text-gray-200 flex flex-row justify-center text-5xl hover:underline {$page
+				.url.pathname === '/'
+				? 'underline'
+				: ''}">Home</a
+		>
+		<a
+			href="/streaming"
+			class="font-serif text-gray-200 flex flex-row justify-center text-5xl hover:underline {$page
+				.url.pathname === '/streaming'
+				? 'underline'
+				: ''}">Upcoming Streamed Movies</a
+		>
+		<a
+			href="/theater"
+			class="font-serif text-gray-200 flex flex-row justify-center text-5xl hover:underline {$page
+				.url.pathname === '/theater'
+				? 'underline'
+				: ''}">Upcoming Theatrical Releases</a
+		>
+	</div>
 
-<div class="px-5"><slot /></div>
-
+	<div class="px-5"><slot /></div>
 </div>
