df1a1c519a351ade076c03db4c7488fd4b85d0b7
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-12-12 20:34:52 -0600

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,8 +26,7 @@
 				? 'underline'
 				: ''}">Theatrical Releases</a
 		>
-		<div>Blu-Ray Releases</div>
-	</div>
+		<div class="font-serif text-gray-200 flex flex-row justify-center text-5xl hover:underline">Blu-Ray Releases</div>
 
 	<div class="px-5"><slot /></div>
 </div>
